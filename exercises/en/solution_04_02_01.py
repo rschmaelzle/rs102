aefe1dbdbbfe48011c213493570edbe9c3db75ab
rschmaelzle <schmaelz@msu.edu>
2020-10-21 11:24:33 -0400

    Update and rename solution_01_02_01.py to solution_04_02_01.py

---
+++ exercises/en/solution_04_02_01.py
@@ -0,0 +1,26 @@
+#import the NetworkX module with the abbreviation nx
+import networkx as nx
+
+
+# define a network
+G_symmetric = nx.Graph()
+
+
+# add edges (as you do this, nodes are also created)
+G_symmetric.add_edge('Peter Miller','Maria Meier')
+G_symmetric.add_edge('Peter Miller','Jonathan Franzen')
+G_symmetric.add_edge('Hans Fallada','Jonathan Franzen')
+G_symmetric.add_edge('Peter Miller','Hans Fallada')
+G_symmetric.add_edge('Peter Miller','Kurt Vonnegut')
+G_symmetric.add_edge('Maria Meier','Jonathan Franzen')
+G_symmetric.add_edge('Maria Meier','Hans Fallada')
+G_symmetric.add_edge('Kurt Vonnegut','Jonathan Franzen')
+G_symmetric.add_edge('Thomas Mann','Maria Meier')
+
+
+# plot the network
+nx.draw_networkx(G_symmetric)
+
+
+# measure the degree of Peter Miller
+nx.degree(G_symmetric, 'Peter Miller')
